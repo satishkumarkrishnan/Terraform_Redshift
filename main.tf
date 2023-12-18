d13f6ea1784b464e0235d2ffc3782537faa7cfcb
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-18 14:38:19 +0530

    added node type to dc2.large

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
 
-
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.0"
+    }
+  }
+}
 
 module "iam" {
   source="git@github.com:satishkumarkrishnan/Terraform_IAM.git?ref=main"  
