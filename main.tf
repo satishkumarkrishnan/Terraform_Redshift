89994e44bfe18946e508d1d5e2e10d97aff9b561
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-19 16:05:00 +0530

    removed final_snapshot_identifier

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_redshift_cluster" "tokyo-redshift-cluster" {
   master_password           = "Tokyo123"
   node_type                 = "dc2.large"
   cluster_type              = "single-node"
-  final_snapshot_identifier = "true"
+  final_snapshot_identifier = "false"
   
   depends_on                = [module.vpc]  
 }
