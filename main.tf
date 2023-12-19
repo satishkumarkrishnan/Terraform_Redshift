a3e133a8ad36f4dd8dfab79f2f10cc1dcb9a568b
satishkumarkrishnan <satishkumar.krishnan83.com>
2023-12-19 08:41:55 +0530

    added new provider version

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version   = "5.0.0"  
-      #version   = "~> 4.2.0"  
+     # version   = "5.0.0"  
+      version   = "~> 4.2.0"  
     }
   }
 }
