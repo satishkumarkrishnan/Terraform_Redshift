37725293322fd8f3fd6cb62f5a64c27da254e48e
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-01-09 11:33:59 +0530

    updated for jan 9th

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "aws_redshift_cluster" "tokyo-redshift-cluster" {
   master_password           = "Tokyo123"
   node_type                 = "dc2.large"
   cluster_type              = "single-node"
-  final_snapshot_identifier = "tokyo-cluster-backup7"
+  final_snapshot_identifier = "tokyo-cluster-backup8"
   depends_on = [aws_default_vpc.default-tokyo-vpc]  
 }
 
